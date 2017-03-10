acfeea0bab87a7610d1efdb0d46f976176cfddb5
aapeshave <peshave.aj@husky.neu.edu>
2017-03-09 19:09:19 -0500

    Adding resources directory. Adding Entity directroy. Adding sample login page

--- release/src/main/java/com/blogit/controllers/LoginController.java
+++ release/src/main/java/com/blogit/controllers/LoginController.java
@@ -1,9 +1,10 @@
 package com.blogit.controllers;
 
+import com.blogit.Entity.LoginEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by ajinkya on 3/8/17.
@@ -14,7 +15,14 @@ public class LoginController {
     @RequestMapping("/login")
     public String login(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-
+        model.addAttribute("loginEntity", new LoginEntity());
         return "login";
     }
+
+    @PostMapping(value = "/login")
+    public String loginAction(@ModelAttribute LoginEntity loginEntity, BindingResult result) {
+        return "result";
+    }
+
 }
+
