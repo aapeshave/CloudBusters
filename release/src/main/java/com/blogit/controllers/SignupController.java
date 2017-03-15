f6f2ac2d80d78ce9d9934431b24f93810cbd689b
ameyutturkar <au.utturkar@gmail.com>
2017-03-15 13:46:22 -0400

    <Signup page created. Reformatted Code>

--- release/src/main/java/com/blogit/controllers/SignupController.java
+++ release/src/main/java/com/blogit/controllers/SignupController.java
@@ -12,19 +12,16 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by ameyutturkar on 3/15/17.
  */
 @Controller
-public class SignupController
-{
+public class SignupController {
     @RequestMapping("/signup")
-    public String signup(@RequestParam(value = "name", required = true, defaultValue = "Name") String name, Model model)
-    {
+    public String signup(@RequestParam(value = "name", required = true, defaultValue = "Name") String name, Model model) {
         model.addAttribute("name", name);
         model.addAttribute("signupEntity", new SignupEntity());
         return "signup";
     }
 
     @PostMapping(value = "/signup")
-    public String signupAction(@ModelAttribute SignupEntity signupEntity)
-    {
+    public String signupAction(@ModelAttribute SignupEntity signupEntity) {
         return "signupSuccess";
     }
 }
