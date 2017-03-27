19403fafa7e7142be914cc8fa3ad3dd2cb270e95
aapeshave <peshave.aj@husky.neu.edu>
2017-03-27 16:19:31 -0400

    Working signup function

--- release/src/main/java/com/blogit/entity/SignupEntity.java
+++ release/src/main/java/com/blogit/entity/SignupEntity.java
@@ -17,6 +17,16 @@ public class SignupEntity
     private String firstName;
     private String lastName;
 
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getUsername()
     {
         return username;
---
+++ release/src/main/java/com/blogit/service/impl/SignUpServiceImpl.java
@@ -0,0 +1,33 @@
+package com.blogit.service.impl;
+
+import com.blogit.entity.SignupEntity;
+import com.blogit.pojo.User;
+import com.blogit.repositories.UserRepository;
+import com.blogit.service.SignUpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ajinkya on 3/27/17.
+ */
+@Service
+public class SignUpServiceImpl implements SignUpService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public String createUserAccount(SignupEntity signupEntity) {
+        User toCreate = new User(signupEntity.getFirstName(),
+                signupEntity.getLastName(),
+                signupEntity.getUsername(),
+                signupEntity.getPassword(),
+                signupEntity.getEmail());
+
+        User saved = userRepository.save(toCreate);
+        if (saved != null) {
+            return saved.getId();
+        }
+        return null;
+    }
+}
