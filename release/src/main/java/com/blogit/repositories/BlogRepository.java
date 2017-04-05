87636ec647a78a71c0551f29cd2810ea00f83ba6
voraankur <vora.a@husky.neu.edu>
2017-04-05 17:30:11 -0400

    BlogRepositoryTest

--- release/src/main/java/com/blogit/repositories/BlogRepository.java
+++ release/src/main/java/com/blogit/repositories/BlogRepository.java
@@ -2,12 +2,14 @@ package com.blogit.repositories;
 
 import java.util.List;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import com.blogit.pojo.Blog;
 
+@EnableScan
 public interface BlogRepository extends CrudRepository<Blog, String> {
 	
-	List<Blog> findByLastName(String blogID);
+	List<Blog> findByUserID(String userID);
 
 }
--- release/src/test/java/com/blogit/BlogRepositoryTest.java
+++ release/src/test/java/com/blogit/BlogRepositoryTest.java
@@ -1,17 +1,50 @@
 package com.blogit;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.blogit.pojo.Blog;
+import com.blogit.pojo.User;
 import com.blogit.repositories.BlogRepository;
+import com.blogit.repositories.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogRepositoryTest {
 	
 	@Autowired
-    private BlogRepository repository;
+    private BlogRepository blogRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+    public void testFindByUserID() throws Exception {
+		List<String> tokens = new ArrayList<>();
+        tokens.add("Sample Token 1");
+
+        User user = new User("Dave", "Matthews");
+        user.setAccessTokens(tokens);
+        userRepository.save(user);
+        
+        Blog blog1 = new Blog("Sports", "All about sports", user.getId());
+        
+        blogRepository.save(blog1);
+        List<Blog> result = blogRepository.findByUserID(blog1.getUserID());
+        Assert.assertNotNull("Assert that result is not empty", result);
+
+        for (Blog blog : result) {
+            System.out.println(blog);
+        }
+        Assert.assertEquals("Assert that only one element is present in the list", 1, result.size());
+        // repository.delete(result);
+	}
 
 }
