61d57efc4903358d27a065da6fe3a65458bddacd
aapeshave <peshave.aj@husky.neu.edu>
2017-04-27 10:53:45 -0400

    Cosmetic changes with object diagram

--- release/src/main/resources/static/js/blog.js
+++ release/src/main/resources/static/js/blog.js
@@ -3,14 +3,12 @@ $(document).ready(function () {
     $('#submit_blog_create').on('click', function () {
 
         $("#blogform").submit(function (event) {
-            alert("Handler for .submit() called.");
             $.ajax({
                 type: "POST",
             url: "/blog",
             data: $("#blogform").serialize(),
                 success: function(data) {
                     $('#created').html(data);
-                alert(data);
             }
         });
             event.preventDefault();
--- release/src/main/resources/static/js/newLogin.js
+++ release/src/main/resources/static/js/newLogin.js
@@ -2,5 +2,4 @@
  * Created by ameyutturkar on 4/15/17.
  */
 function callJS() {
-    alert("Javascript for login called from external JS file");
 }
